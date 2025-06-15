30c4d367bc4d178b6f3c4156121381733eca51a9
TanishTushid <tanish.tushid.2004@gmail.com>
2025-06-15 20:48:42 +0530

    Add files via upload

---
+++ any_all.py
@@ -0,0 +1,5 @@
+N = int(input())
+Numbers = list(map(int, input().split()))
+all_positive = all(x>0 for x in Numbers)
+any_palindromic = any(str(x)==str(x)[::-1] for x in Numbers)
+print(all_positive and any_palindromic)
